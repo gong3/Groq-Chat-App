1e5f14f613becef952194e8a6239f4e7ca358d09
GongKung <gong_cyber@hotmail.com>
2024-03-28 10:19:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ groq_api_key = os.environ['GROQ_API_KEY']
 
 def main():
 
-    st.title("Groq Chat App")
+    st.title("Gong Kung Chat App จร้า")
 
     # Add customization options to the sidebar
     st.sidebar.title('Select an LLM')
